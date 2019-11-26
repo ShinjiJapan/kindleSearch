2a8b80d24d1fef978e142f851339441a0ccb970a
shinji tanaka <shinji.japan@live.jp>
2019-11-26 19:51:51 +0900

    詳細エリアの表示状態を保存

--- src/components/toolbar/DetailAreaVM.ts
+++ src/components/toolbar/DetailAreaVM.ts
@@ -1,9 +1,15 @@
 import { BindableBase } from "../../BindableBase";
 
 export default class extends BindableBase {
+  public constructor() {
+    super();
+    this.isOpen = localStorage.getItem("IsOpenDetailArea") === "true";
+  }
+
   public isOpen = false;
   public onClick = () => {
     this.isOpen = !this.isOpen;
+    localStorage.setItem("IsOpenDetailArea", this.isOpen.toString());
     this.onPropertyChanged();
   };
 }
